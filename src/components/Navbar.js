14cea11c787151f0507a51a396ae5adba1e76e54
Elaheh <elah0048@stud.kea.dk>
2022-11-16 20:19:59 +0100

    done

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+ 
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Blogs</li>
+      </ul>
+    </nav>
+  );
+}
+
+
+export default Navbar;
---
+++ src/components/ScrollIndicator.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import "./ScrollIndicator.css";
+
+
+function ScrollIndicator() {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const onScroll = () => {
+    const winScroll = document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    setScrollTop(scrolled);
+  };
+  
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+  });
+
+  return (
+    <div className="wrapper">
+        <div className="progressbar">
+            <div className="progressbar-main" style={{ width: `${scrollTop}%`}}></div>
+        </div>
+    </div>
+  )
+
+}
+
+  export default ScrollIndicator;
